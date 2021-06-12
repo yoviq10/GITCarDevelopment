1d6c4db151e196ec81413f436e39d79f2a52d6a7
yoviq10 <quisbert.yovanna@gmail.com>
2021-06-12 13:12:15 -0400

    final red audit car at your services

--- src/company/Main.java
+++ src/company/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args){
 
         car audi=new car();
+        audi.color="Red";
+        audi.number="111";
+        audi.door="2";
+        audi.model="A3";
 
 
 
