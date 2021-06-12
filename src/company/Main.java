6ac6ac0dbd998910929c3e8cbde4b0b0254be2c3
yoviq10 <quisbert.yovanna@gmail.com>
2021-06-12 11:14:42 -0400

    basic features of the car added and car object created

---
+++ src/company/Main.java
@@ -0,0 +1,14 @@
+package company;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        car audi=new car();
+
+
+
+
+    }
+
+}
---
+++ src/company/car.java
@@ -0,0 +1,11 @@
+package company;
+
+public class car {
+
+    String color;
+    String number;
+    String model;
+    String door;
+
+
+}
